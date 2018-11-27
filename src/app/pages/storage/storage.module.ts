8fef321a3657479b2043375ab5a78639861963a2
Lola Yang <lola.ly27@gmail.com>
2018-11-27 13:32:07 -0800

    installed and import filesize moudle

--- src/app/pages/storage/storage.module.ts
+++ src/app/pages/storage/storage.module.ts
@@ -7,6 +7,7 @@ import { MaterialModule } from '../../appMaterial.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {FileSizeModule} from 'ngx-filesize';
 
 import { EntityModule } from '../common/entity/entity.module';
 import { UserService } from '../../services/user.service';
@@ -47,7 +48,8 @@ import { MessageService } from '../common/entity/entity-form/services/message.se
   imports : [
     RouterModule, DragulaModule, EntityModule, CommonModule, FormsModule,
     ReactiveFormsModule, routing, MaterialModule, TreeTableModule,
-    NgxDatatableModule, DxTreeListModule, TranslateModule,FlexLayoutModule
+    NgxDatatableModule, DxTreeListModule, TranslateModule,FlexLayoutModule,
+    FileSizeModule
   ],
   declarations : [
     VolumesListComponent,
