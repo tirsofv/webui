e6155bc4d6bd1c9f3bd9c18e4f88735d281427bf
Dennis Mullen <dkmullen@gmail.com>
2019-01-17 09:54:19 -0500

    Match settings to 11.3

--- src/app/pages/common/entity/entity-table/entity-table.component.ts
+++ src/app/pages/common/entity/entity-table/entity-table.component.ts
@@ -152,7 +152,7 @@ export class EntityTableComponent /*extends ViewControllerComponent*/ implements
       if (this.conf.afterInit) {
         this.conf.afterInit(this);
       }
-    }, 25)
+    })
   
     this.conf.columns.forEach((column) => {
       this.displayedColumns.push(column.prop);
@@ -365,12 +365,12 @@ export class EntityTableComponent /*extends ViewControllerComponent*/ implements
     this.showDefaults = true;
 
     // Here's a hack temporarily adjust the page and fix the ngx layout - works maybe 80% of time
-    setTimeout(() => {
-      document.body.style.width = window.innerWidth -1 + 'px';
-      setTimeout(() => {
-        document.body.style.width = 'auto';
-      }, 100)
-    }, 20);
+    // setTimeout(() => {
+    //   document.body.style.width = window.innerWidth -1 + 'px';
+    //   setTimeout(() => {
+    //     document.body.style.width = 'auto';
+    //   }, 100)
+    // }, 20);
     // end of hack
 
         return res;
